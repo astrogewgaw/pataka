3eecc900e26b35b8e30b742df4eca0a2a07204d7
astrogewgaw <ujjwalpanda97@gmail.com>
2021-09-16 23:39:14 +0530

    :art: Minor arg name change for `noise`.

--- src/pataka/noise.py
+++ src/pataka/noise.py
@@ -15,8 +15,8 @@ import numpy as np
 
 
 def noise(
-    nsamp: int,
-    nchan: int = 1,
+    nt: int,
+    nf: int = 1,
     beta: float = 2.0,
     cutoff: float = 0.0,
 ) -> np.ndarray:
@@ -25,8 +25,8 @@ def noise(
     Generate power law noise.
 
     Args:
-        nsamp:  The number of samples in the time series.
-        nchan:  The number of frequency channels. If nchan > 1, the noise being
+        nt:     The number of samples in the time series.
+        nf:     The number of frequency channels. If nf > 1, the noise being
                 generated is a two-dimensional array, which is what we need for
                 simulating frequency-time data. This is what is recorded by most
                 radio telescopes for offline processing. For real-time processing,
@@ -35,41 +35,41 @@ def noise(
         beta:   The spectral index of the noise. Since red noise is encountered more
                 often in pulsar astronomy, this value is set to 2.0 by default.
         cutoff: The low frequency cutoff. The default value of 0.0 can be used to
-                reproduce the analysis in the original paper. If cutoff < 1 / nsamp,
-                its value is fixed to that limit, that is, cutoff =  1 / nsamp.
+                reproduce the analysis in the original paper. If cutoff < 1 / nt,
+                its value is fixed to that limit, that is, cutoff =  1 / nt.
     """
 
-    freqs = np.fft.rfftfreq(nsamp)
+    freqs = np.fft.rfftfreq(nt)
 
     scales = freqs.copy()
-    cutoff = max(cutoff, 1.0 / nsamp)
+    cutoff = max(cutoff, 1.0 / nt)
     islice = np.sum(scales < cutoff)
     if islice and (islice < scales.size):
         scales[:islice] = scales[islice]
     scales = scales ** (-beta / 2.0)
 
     weights = scales[1:].copy()
-    weights[-1] *= (1 + (nsamp % 2)) / 2.0
-    stddev = 2 * np.sqrt(np.sum(weights ** 2.0)) / nsamp
+    weights[-1] *= (1 + (nt % 2)) / 2.0
+    stddev = 2 * np.sqrt(np.sum(weights ** 2.0)) / nt
 
-    sizes = (nchan, freqs.size)
-    extras = 0 if nchan == 1 else 1
+    sizes = (nf, freqs.size)
+    extras = 0 if nf == 1 else 1
     scales = scales[(np.newaxis,) * extras + (Ellipsis,)]
 
     srpow = np.random.normal(scale=scales, size=sizes)
     srphz = np.random.normal(scale=scales, size=sizes)
 
-    if not (nsamp % 2):
+    if not (nt % 2):
         srphz[..., -1] = 0.0
     srphz[..., 0] = 0.0
 
     noise = (
         np.fft.irfft(
             srpow + 1j * srphz,
-            n=nsamp,
+            n=nt,
             axis=-1,
         )
         / stddev
     )
 
-    return noise[0] if nchan == 1 else noise
+    return noise[0] if nf == 1 else noise
